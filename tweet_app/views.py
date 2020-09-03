cf4d7a7e31294e6310a4d3dc15a81f8c1a4cb69d
Kevin Blount <kvbchamp@gmail.com>
2020-09-03 12:52:07 -0400

    add text labels for tweet url page

--- tweet_app/views.py
+++ tweet_app/views.py
@@ -23,5 +23,5 @@ def create_tweet(request):
 
 
 def tweet_detail_view(request, tweet_id):
-    tweet_detail = TweetModel.objects.filter(id=tweet_id)
+    tweet_detail = TweetModel.objects.filter(id=tweet_id).first()
     return render(request, 'tweet_detail.html', {"tweet": tweet_detail})
